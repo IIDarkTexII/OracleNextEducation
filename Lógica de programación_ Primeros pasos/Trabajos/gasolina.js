d10546541f73b8fa60b440019687e9730333b9bd
Alejandro Texis <texis369alejandro@gmail.com>
2022-12-01 22:45:58 -0600

    Semana 1

---
+++ Lógica de programación_ Primeros pasos/Trabajos/gasolina.js
@@ -0,0 +1,10 @@
+const tanque= 40;
+var gas= 480;
+var alcohol= 300;
+
+var consumoG= gas/tanque;
+var consumoA= alcohol/tanque;
+
+document.write("El rendimiento que tiene su auto con gasolina es de: " + consumoG + " km/hr");
+document.write("<br>");
+document.write("El rendimiento que tiene su auto con alcohol es de: " + consumoA + " km/hr");
